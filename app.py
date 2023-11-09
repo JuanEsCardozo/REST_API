ad5eb922a83ae9da7fb0e389c2d4ec6e28f3d733
Juan Cardozo <juanescardozo22@gmail.com>
2023-11-09 02:49:46 -0500

    creating a function that downloads csv files from Cloud Storage

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from google.cloud import storage, secretmanager
 from flask import Flask
+from io import BytesIO
+from config import *
+import pandas as pd
+import io
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +21,23 @@ def access_secret_key(project_id, secret_id, version_id):
     
     except Exception as e:
         print("An error occurred:", str(e))
+
+def get_csv_file(blob_name, bucket_name):
+
+    try:
+        json_key = json.loads(access_secret_key(PROJECT_ID, SECRET_ID, VERSION))
+
+        storage_client = storage.Client.from_service_account_info(json_key)
+
+        bucket = storage_client.get_bucket(bucket_name)
+        csv_data = pd.read_csv(io.BytesIO(bucket.blob(blob_name).download_as_string()), encoding='UTF-8', sep=',', index_col=None)
+        df = pd.DataFrame(csv_data)
+        print(csv_data)
+        return df
+
+    except Exception as e:
+        print(e)
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
